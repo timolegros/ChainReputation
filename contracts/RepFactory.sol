728af7121fc6da4860cb3ed58bbc36adede7b56a
timolegros <timothee@ualberta.ca>
2021-07-05 16:59:44 +0200

    v3 init

--- contracts/RepFactory.sol
+++ contracts/RepFactory.sol
@@ -1,7 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-contract RepFactory {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IRepFactory {
+  /**
+  * @notice This struct represents a single reputation Token type
+  * @dev The CID is an IPFS CID that stores the token standards and the oracles address array stores the oracles that
+  * the token owner "trusts".
+  */
+  struct Token { bytes CID; address[] oracles; address owner; }
+
+
+}
+
+contract RepFactory is Ownable{
   constructor() public {
   }
 }
